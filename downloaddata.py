1df2840b3f0679ac1a0cd66928b6b5a47115f7a4
xuxiao2695 <xuxiao2695@gmail.com>
2017-06-28 15:57:38 -0400

    Update downloaddata.py

--- downloaddata.py
+++ downloaddata.py
@@ -2,8 +2,10 @@ import quandl
 
 
 def get_quandl_data(symbollist, startdate, enddate):
+    # startdate='2017-06-27', symbollist=['AAPL','TSLA']
     quandl.ApiConfig.api_key = "PmL3cEPezsZ_dAsyxrvX"
     df = quandl.get("WIKI/" + symbollist[0] + ".11", start_date=startdate, end_date=enddate)
+    # "WIKI/AAPL.11"
     df.columns = [symbollist[0]]
     for stock in symbollist[1:]:
         df[stock] = quandl.get("WIKI/" + stock + ".11", start_date=startdate, end_date=enddate)
