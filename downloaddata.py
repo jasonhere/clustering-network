91f67a7c476b471b4f0c0d2106a367248c040c51
xuxiao2695 <xuxiao2695@gmail.com>
2017-06-29 10:09:58 -0400

    Downloaded SP500 full history to SP500_full.csv.

--- downloaddata.py
+++ downloaddata.py
@@ -4,9 +4,11 @@ import quandl
 def get_quandl_data(symbollist, startdate, enddate):
     # startdate='2017-06-27', symbollist=['AAPL','TSLA']
     quandl.ApiConfig.api_key = "PmL3cEPezsZ_dAsyxrvX"
+    print "downloading " + symbollist[0]
     df = quandl.get("WIKI/" + symbollist[0] + ".11", start_date=startdate, end_date=enddate)
     # "WIKI/AAPL.11"
     df.columns = [symbollist[0]]
     for stock in symbollist[1:]:
+        print "downloading " + stock
         df[stock] = quandl.get("WIKI/" + stock + ".11", start_date=startdate, end_date=enddate)
     return df
